65e392e811ece5b7237242bb7d9922d61f57dcfb
EstebanMarin <estebanmarin002@gmail.com>
2026-02-19 19:40:37 -0500

    more chaopter 2

--- NaturalNumbers/MyNat.lean
+++ NaturalNumbers/MyNat.lean
@@ -1,6 +1,37 @@
+import Mathlib.Data.Nat.Basic
+import Mathlib.Tactic
+
 inductive MyNat: Type where 
   | zero : MyNat 
   | succ : MyNat → MyNat
 
 inductive AExp: Type where 
   | num: ℤ → AExp
+  | var: String → AExp
+  | add: AExp → AExp → AExp
+  | sub: AExp → AExp → AExp
+  | mul: AExp → AExp → AExp
+  | div: AExp → AExp → AExp
+
+inductive MyList (α : Type) where
+  | nil : MyList α
+  | cons : α → MyList α → MyList α
+
+#check MyList.cons
+
+def add: ℕ → ℕ → ℕ
+  | m, Nat.zero => m
+  | m, Nat.succ n => Nat.succ (add m n)
+
+
+#eval add 2 7 
+
+#reduce add 2 7
+
+def mul: ℕ → ℕ → ℕ 
+  | _, Nat.zero =>  Nat.zero
+  | m, Nat.succ n => add m (mul m n)
+
+def power: ℕ → ℕ → ℕ 
+  | _, Nat.zero => 1
+  | m, Nat.succ n => mul m (power m n)
